1f3acbceb5f2f4de8d0b1882c0db2c8d1596f6ed
Vollmer Max <maximilian.vollmer2708@gmail.com>
2023-12-20 10:26:00 +0100

    first commit

---
+++ src/test/java/com/example/hackncrawl/HacknCrawlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackncrawl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacknCrawlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
