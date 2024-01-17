50649f442a276ff13f6633410d7792fe2602b2fc
Vollmer Max <maximilian.vollmer2708@gmail.com>
2024-01-17 21:43:39 +0100

    Add Coop features

---
+++ src/main/java/com/example/hackncrawl/Domain/CardPlayed.java
@@ -0,0 +1,14 @@
+package com.example.hackncrawl.Domain;
+
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloMessage {
+    private String name;
+}
